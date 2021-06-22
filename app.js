7bdfe0e1cd1419d1ef6a15766f38780ac4a0fd28
Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-06-22 19:06:33 +0100

    removed irrelevances

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 const port = process.env.PORT || 8089;
-const mongoose = require('mongoose');
 app.use(express.static('public'));
 
 

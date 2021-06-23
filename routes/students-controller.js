644a607d68473a0883a670bc7e5c08bd523c076e
Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-06-23 12:41:40 +0100

    view engine setup

--- routes/students-controller.js
+++ routes/students-controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/student', (req,res,next)=>{
-    res.json('Made First Request...')
+    res.render('pages/home')
 })
 
 module.exports = router;

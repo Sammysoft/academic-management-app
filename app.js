9f2c815958cfdb7470abf54dd16945bf69d1983c
Dare-Owonibi <dareowonibisamuel@gmail.com>
2021-06-23 12:20:55 +0100

    added view engine

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const hbs = require('express-handlebars');
 const port = process.env.PORT || 8089;
 app.use(express.static('public'));
 
@@ -11,6 +13,13 @@ require('./model/student-model');
 app.listen(port, '0.0.0.0', ()=>{
     console.log(`Server running on port ${port}`)
 });
+//Setting up view engine: Handlebars
+
+
+//Setting up Views Engine
+app.engine('hbs', hbs({extname:'hbs', defaultLayout: 'MainLayout',  layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, '/views/'));
+app.set('view engine', 'hbs');
 
 
 app.use('/', student_controller)
